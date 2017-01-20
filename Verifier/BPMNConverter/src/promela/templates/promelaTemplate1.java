deb05c52d9f4af6d7c835b08dc41837b080222b4
jvisker <jamievisker@gmail.com>
2017-01-19 19:55:17 -0700

    change

--- Verifier/BPMNConverter/src/promela/templates/promelaTemplate1.java
+++ Verifier/BPMNConverter/src/promela/templates/promelaTemplate1.java
@@ -37,16 +37,15 @@ public class promelaTemplate1 {
 	public String getFoundationalStructure(String processChannels, String processInitializers) {
 		String s = "#define N 3\n";
 		s += "mtype = {taskID}\n";
-
+		s += "#include split_and_gate.pml\n";
 		// s += processProctypes;
-		s += this.getHelperProctypes();
 
 		s += "init {\n";
 		s += processChannels;
 
 		s += "chan done = [N] of {byte};\n";
 		s += "chan terminate = [1] of {bool};\n";
-		s += "chan and_gate = [1] of {byte};\n";
+
 		s += "\n";
 		s += processInitializers;
 		s += "\n";
@@ -55,80 +54,42 @@ public class promelaTemplate1 {
 		s += "do\n";
 		// s += ":: nfull(in) ->\n";
 
-		s += ":: nfull(processChannelstart) ->\n";
-		s += "atomic {\n";
-		s += "select(x : 1 .. N);\n";
-		s += "printf(\"send(%d)\\n\", x);\n";
+		s += "  :: nfull(processChannelstart) ->\n";
+		s += "    atomic {\n";
+		s += "      select(x : 1 .. N);\n";
+		s += "      printf(\"send(%d)\\n\", x);\n";
 		// s += "in!x;\n";
 		// replace with more generic solution
-		s += "processChannelstart!x;\n";
+		s += "      processChannelstart!x;\n";
 
-		s += "}\n";
-		s += ":: nfull(done) ->\n";
-		s += "atomic {\n";
-		s += "select(x : 1 .. N);\n";
-		s += "if\n";
-		s += ":: !(done??[eval(x)]) ->\n";
-		s += "done!!x;\n";
-		s += "printf(\"done(%d)\\n\", x)\n";
-		s += ":: else -> skip\n";
-		s += "fi\n";
-		s += "}\n";
-		s += ":: nfull(terminate) ->\n";
-		s += "atomic {\n";
-		s += "printf(\"terminate\\n\")\n";
-		s += "terminate!true;\n";
-		s += "}\n";
+		s += "    }\n";
+		s += "  :: nfull(done) ->\n";
+		s += "    atomic {\n";
+		s += "    select(x : 1 .. N);\n";
+		s += "    if\n";
+		s += "      :: !(done??[eval(x)]) ->\n";
+		s += "        done!!x;\n";
+		s += "        printf(\"done(%d)\\n\", x)\n";
+		s += "      :: else -> skip\n";
+		s += "    fi\n";
+		s += "    }\n";
+		s += "  :: nfull(terminate) ->\n";
+		s += "    atomic {\n";
+		s += "      printf(\"terminate\\n\")\n";
+		s += "      terminate!true;\n";
+		s += "    }\n";
 		/*
 		 * s += ":: out?[_] ->\n"; s += "atomic {\n"; s += "out?x;\n"; s +=
 		 * "printf(\"receive(%d)\\n\", x);\n"; s += "}\n";
 		 */
 		s += "od;\n";
 		s += "} unless {\n";
-		s += "if\n";
-		s += ":: terminate?[_] -> printf(\"terminate(%d)\\n\", _pid);\n";
-		s += "fi;\n";
+		s += "  if\n";
+		s += "    :: terminate?[_] -> printf(\"terminate(%d)\\n\", _pid);\n";
+		s += "  fi;\n";
 		s += "}\n";
 		s += "};\n";
 		return s;
-	}
-
-	private String getHelperProctypes() {
-		// TODO Auto-generated method stub
-
-		String s = "proctype split_and_gate(chan in, out1, out2, done, terminate; mtype id) {\n";
-		s += "  byte x = 0, i = 0;\n";
-		s += "  bool send1 = false, send2 = false;\n";
-		s += "L0:\n";
-		s += "  atomic {\n";
-		s += "	in?x;\n";
-		s += "	printf(\"%e(%d)\\n\", taskID, x);\n";
-		s += "	if\n";
-		s += "	  :: !(done??[eval(x)]) ->\n";
-		s += "		 send1 = false;\n";
-		s += "		 send2 = false;\n";
-		s += "		 do\n";
-		s += "		   :: nfull(out1) ->\n";
-		s += "			  out1!x;\n";
-		s += "			  send1 = true;\n";
-		s += "		   :: nfull(out2) -> \n";
-		s += "			  out2!x;\n";
-		s += "			  send2 = true;\n";
-		s += "		   :: send1 && send2 ->\n";
-		s += "			  break;\n";
-		s += "		 od;		 \n";
-		s += "		 printf(\"send(%d)\\n\", x);\n";
-		s += "	  :: done??[eval(x)] ->\n";
-		s += "		 printf(\"done(%d)\\n\", x);\n";
-		s += "	fi;\n";
-		s += "	goto L0;\n";
-		s += "  } unless {\n";
-		s += "	if\n";
-		s += "	  :: terminate?[_] -> printf(\"terminate(%d)\\n\", _pid);\n";
-		s += "	fi;\n";
-		s += "  }\n";
-		s += "}\n";
-		return "";
 	}
 
 	public String getTaskTemplate(String processName) {
